a44b2516ec86790f3b29004a1efb1d053bb329b4
Kirsten Scherer Auberson <kirsten.scherer@gmail.com>
2018-07-23 22:42:23 +0200

    Use new sampler implementation: The same entry can't be in both training- and test-set

--- src/main/java/net/auberson/scherer/masterthesis/Experiment1.java
+++ src/main/java/net/auberson/scherer/masterthesis/Experiment1.java
@@ -14,7 +14,7 @@ import net.auberson.scherer.masterthesis.util.Sampler;
  */
 public class Experiment1 extends ExperimentBase implements Runnable {
 
-	public static final File DATA_DIR = new File("./data/processed/experiment1");
+	public static final File DATA_DIR = new File("./data/processed/ex1");
 
 	private static final int TRAINING_SET_SIZE = 150;
 	private static final int TEST_SET_SIZE = 600;
@@ -41,10 +41,12 @@ public class Experiment1 extends ExperimentBase implements Runnable {
 
 		File trainingSet = getEmptyFile(DATA_DIR, "Iteration", "0", "Training");
 		System.out.println("Creating training set in " + trainingSet.getPath());
-		Sampler.sample(TRAINING_SET_SIZE, classNames, sampleCount, trainingSet);
-
-		System.out.println("Training initial Classifier with " + TRAINING_SET_SIZE + " samples");
-		File output = trainAndClassify(trainingSet, 0);
+		
+		File testSet = getEmptyFile(DATA_DIR, "Iteration", "0", "Test");
+		System.out.println("Creating test set in " + testSet.getPath());
+		
+		Sampler.sample(new int[]{TRAINING_SET_SIZE, TEST_SET_SIZE}, classNames, sampleCount, trainingSet, testSet);
+		File output = trainAndClassify(trainingSet, testSet, TRAINING_SET_SIZE, 0);
 
 		// This list simulates the entries that would have been manually reviewed:
 		List<Element> reviewedEntries = new ArrayList<Element>();
@@ -66,21 +68,21 @@ public class Experiment1 extends ExperimentBase implements Runnable {
 			trainingSet = getEmptyFile(DATA_DIR, "Iteration", Integer.toString(i), "Training");
 			System.out.println("Creating training set in " + trainingSet.getPath());
 			outputSamples(reviewedEntries, trainingSet);
-			int remainingSamples = TRAINING_SET_SIZE + Math.floorDiv(-reviewedEntries.size(), classCount);
-			remainingSamples = Math.max(remainingSamples, 0);
-			Sampler.sample(remainingSamples, classNames, sampleCount, trainingSet);
-
-			output = trainAndClassify(trainingSet, i);
+			int missingTrainingSetSample = TRAINING_SET_SIZE + Math.floorDiv(-reviewedEntries.size(), classCount);
+			missingTrainingSetSample = Math.max(missingTrainingSetSample, 0);
+			
+			testSet = getEmptyFile(DATA_DIR, "Iteration", Integer.toString(i), "Test");
+			System.out.println("Creating test set in " + testSet.getPath());
+			
+			Sampler.sample(new int[]{missingTrainingSetSample, TEST_SET_SIZE}, classNames, sampleCount, trainingSet, testSet);
+			output = trainAndClassify(trainingSet, testSet, reviewedEntries.size()+missingTrainingSetSample, i);
 		}
 
 	}
 
-	private File trainAndClassify(File input, Integer iter) {
-		BatchClassifier classifier = trainClassifier(input, "Ex1", "Iteration" + iter.toString());
-
-		File testSet = getEmptyFile(DATA_DIR, "Iteration", iter.toString(), "Test");
-		System.out.println("Creating test set in " + testSet.getPath());
-		Sampler.sample(TEST_SET_SIZE, classNames, sampleCount, testSet);
+	private File trainAndClassify(File trainingSet, File testSet, int trainingSetSize, Integer iter) {
+		System.out.println("Training Classifier with " + trainingSetSize + " samples");
+		BatchClassifier classifier = trainClassifier(trainingSet, "Ex1", "Iteration" + iter.toString());
 
 		File output = getEmptyFile(DATA_DIR, "Iteration", iter.toString(), "Output");
 		System.out.println("Classifying test set into " + output.getPath());
