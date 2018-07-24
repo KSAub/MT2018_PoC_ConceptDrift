7a32f6f92aae09e3c77d099799936f866e0d5923
Frederic Auberson <frederic@auberson.net>
2018-07-24 22:38:56 +0200

    Fixed execution script

--- run.sh
+++ run.sh
@@ -7,15 +7,17 @@ cd "$(dirname "$0")"
 case $1 in
 	
     extract)
-    java -classpath "./target/*" -Xmx4G -XX:+UseParallelGC -XX:-UseGCOverheadLimit -DentityExpansionLimit=0 -DtotalEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 net.auberson.scherer.masterthesis.Extract
+    java -classpath "./target/MasterThesisKSA.jar" -Xmx4G -XX:+UseParallelGC -XX:-UseGCOverheadLimit -DentityExpansionLimit=0 -DtotalEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 net.auberson.scherer.masterthesis.Extract
     ;;
     
     learningcurve)
-    java -classpath "./target/*" net.auberson.scherer.masterthesis.ComputeLearningCurve
+    shift
+    java -classpath "./target/MasterThesisKSA.jar" net.auberson.scherer.masterthesis.ComputeLearningCurve $@
     ;;
     
     experiment1)
-    java -classpath "./target/*" net.auberson.scherer.masterthesis.Experiment1
+    shift
+    java -classpath "./target/MasterThesisKSA.jar" net.auberson.scherer.masterthesis.Experiment1 $@
     ;;
     
     
@@ -27,12 +29,12 @@ case $1 in
     echo "    Generates many dataset CSVs in ./data/intermediate (one CSV per class,"
     echo "    and a CSV containing the dataset sizes)."
     echo "- Compute Learning Curve: $0 learningcurve <categories>"
-    echo "    E.g. $0 learningcurve electronics gaming security travel cooking"
+    echo "    E.g. $0 learningcurve electronics gaming security travel cooking electronics"
     echo "    Trains a classifier for the given categories (the corresponding CSV in "
     echo "    ./data/intermediate must exist) with varying training set sizes, and "
     echo "    outputs the Accuracy for each size to a CSV in ./reports/learning-curve"     
     echo "- Run first experiment: $0 experiment1 <categories>"
-    echo "    E.g. $0 learningcurve experiment1 gaming security travel cooking"
+    echo "    E.g. $0 experiment1 gaming security travel cooking electronics"
     ;;
 esac
 
